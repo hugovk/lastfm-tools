8ccb37d89620123e5265ac0f9fee07c1d84db72f
hugovk <hugovk@users.noreply.github.com>
2015-01-19 13:38:43 +0200

    Add option to unscrobble many at once. Useful, for example, when you leave the scrobbler running all weekend by mistake.

--- unscrobble.py
+++ unscrobble.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-Unscrobble the last played track
+Unscrobble the last played track or tracks
 Prerequisites:
-mylast.py, lastplayed.py, pyLast from https://github.com/hugovk/pylast
+mylast.py, lastplayed.py, pyLast >= 1.0.0
 """
+import argparse
 import pylast
 import sys
 from mylast import *
@@ -45,17 +46,10 @@ def query_yes_no(question, default="yes"):
                              "(or 'y' or 'n').\n")
 
 
-print "Last scrobbles:"
-last_scrobble = get_recent_tracks(lastfm_username, 5)[0]
-
-answer = query_yes_no("Unscrobble most recent?")
-if not answer:
-    sys.exit("Left scrobbled")
-else:
-    my_library = pylast.Library(user=lastfm_username, network=lastfm_network)
-    artist = str(last_scrobble.track.artist)
-    title = last_scrobble.track.title
-    timestamp = last_scrobble.timestamp
+def unscrobble(library, scrobble):
+    artist = str(scrobble.track.artist)
+    title = scrobble.track.title
+    timestamp = scrobble.timestamp
 
     try:
         my_library.remove_scrobble(
@@ -64,13 +58,37 @@ else:
         print "Exception: " + str(e)
         sys.exit(
             "Error: pylast 0.5.11 does not support removing scrobbles. "
-            "Please install latest pylast from "
-            "https://github.com/hugovk/pylast")
+            "Please install latest pylast: pip install -U pylast")
     except Exception as e:
         sys.exit("Exception: " + str(e))
     print "Scrobble removed"
 
-print "Last few are now:"
-get_recent_tracks(lastfm_username, 5)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Unscrobble the last played track or tracks",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-u', '--username', default=lastfm_username,
+        help="Last.fm username")
+    parser.add_argument(
+        '-n', '--number', default=1, type=int,
+        help="Number of tracks to unscrobble")
+    args = parser.parse_args()
+
+    print "Last scrobbles:"
+    last_scrobbles = get_recent_tracks(lastfm_username, args.number)
+
+    answer = query_yes_no("Unscrobble last " + str(args.number) + "?")
+    if not answer:
+        sys.exit("Scrobble kept")
+    else:
+        my_library = pylast.Library(user=lastfm_username, network=lastfm_network)
+
+        for last_scrobble in last_scrobbles:
+            unscrobble(my_library, last_scrobble)
+
+    print "Last few are now:"
+    get_recent_tracks(lastfm_username, 5)
 
 # End of file
