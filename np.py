b5251c8fa226a273416b6bef32c942c5a40c3635
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-05-02 17:16:45 +0300

    Return early if no track

--- np.py
+++ np.py
@@ -80,13 +80,9 @@ def main() -> None:
 
     if not args.loop:
         now_playing = lastfm_network.get_user(args.username).get_now_playing()
-        if type(now_playing) is pylast.Track:
-            output(is_track_loved(now_playing))
-            if args.say:
-                say(now_playing)
-        else:
-            output("No track currently playing")
-
+        if not now_playing:
+            return
+        output(is_track_loved(now_playing))
     else:
         last_played = None
         while True:
